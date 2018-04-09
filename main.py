3e061ada31a122f1e0babde9527b5da66026d218
samambaman <felipe.gomes.ferreira@gmail.com>
2018-04-09 16:55:44 -0300

    alteracao para iniciar processo de multithread

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ from extrator.settings import (
     DS_EXADATA_SID,
     DS_EXADATA_user,
     DS_EXADATA_password)
-# from multiprocessing import Pool
+from multiprocessing import Pool
+
+
+POOLCOUNT = 10
 
 
 def main():
@@ -22,7 +25,8 @@ def main():
         convert_unicode=False,
         pool_recycle=10,
         pool_size=50,
-        echo=True
+        echo=True,
+        isolation_level="AUTOCOMMIT"
     )
 
     dsn_tns = cx_Oracle.makedsn(
@@ -35,29 +39,21 @@ def main():
 
     docs = obter_documentos_externos()
 
-    for doc in docs:
-        try:
-            documento = pipeline(doc[0])
-            if documento == {}:
-                continue
-            atualizar_documento(documento, doc[1])
-        except Exception as error:
-            atualizar_vista(doc[0], doc[1])
+    pool = Pool(POOLCOUNT)
 
-    # pool = Pool(POOLCOUNT)
+    resultados = pool.map(processar_armazenar, docs[0:1000])
 
-    # resultados = pool.map(processar_armazenar, docs[0:1000])
-
-    # print(resultados)
+    print(resultados)
 
 
 def processar_armazenar(documento):
     try:
-        pipeline(documento)
-    except Exception as ex:
-        return {'documento': documento, 'resultado': ex}
-    # aqui armazena no banco e blah
-    return {'documento': documento, 'resultado': 'Yay'}
+        documento = pipeline(documento[0])
+        if documento == {}:
+            return
+        atualizar_documento(documento, documento[1])
+    except Exception as error:
+        atualizar_vista(documento[0], documento[1])
 
 
 if __name__ == '__main__':
